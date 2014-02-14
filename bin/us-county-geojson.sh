61e1ac3908a967a6fa3663c27d2dfcdef60ea0dc
Justin York <justin@genealogysystems.com>
2014-02-14 23:09:35 +0000

    Try highland for tiger counties import

---
+++ bin/us-county-geojson.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Unzips a .zip download from NHGIS,
+# unzips all .zips inside, and runs
+# the ogr2ogr conversion on all resulting
+# .shp files to generate .geojson files
+
+# Make sure a valid file was given as the first argument
+[[ -e $1 ]] || { 
+  echo "First argument must be a valid file; '$1' given"; 
+  exit 1; 
+}
+
+basedir=$(dirname $1)
+zipname=$(basename $1)
+extension="${zipname##*.}"
+zipname="${zipname%.*}"
+
+# Make sure the file is a .zip
+[[ $extension = "zip" ]] || {
+  echo "File must be a .zip";
+  exit 1;
+}
+
+# Unzip file if a directory for it doesn't already exist
+[[ -d "$basedir/$zipname" ]] || {
+  unzip $1 -d $basedir;
+}
+
+# Unzip all resulting .zips if they haven't been already
+for f in $(find $basedir/$zipname -type f -name "*.zip"); do
+  fname=$(basename $f)
+  [[ -f "$basedir/$zipname/${fname%.*}.shp" ]] || unzip $f -d $basedir/$zipname;
+done
+
+# Make sure output directory exists
+[[ -d "$basedir/$zipname/geojsons" ]] || {
+  mkdir "$basedir/$zipname/geojsons"
+}
+
+# Convert all .shp to .geojson
+for f in $(find $basedir/$zipname -type f -name "*.shp"); do
+  fname=$(basename $f)
+  fname="${fname%.*}"
+  echo "Creating $fname.geojson"
+  ogr2ogr -f GeoJSON -s_srs EPSG:102003 -t_srs WGS84 -a_srs WGS84 $basedir/$zipname/geojsons/$fname.geojson $basedir/$zipname/$fname.shp
+done
--- import/us-tiger-counties.js
+++ import/us-tiger-counties.js
@@ -2,6 +2,71 @@
  * Download county GIS files from https://data2.nhgis.org/main.
  * 1. Select the "County (by State)" Geographic level in the upper left filters.
  * 2. Use the "GIS boundary files" tab below to browse through and select files by year (use the latest TIGER/Line basis available).
- * 3. Convert with:
- *      ogr2ogr -f GeoJSON -s_srs EPSG:102003 -t_srs WGS84 -a_srs WGS84 <outputfile>.geojson <inputfile>.shp
+ * 3. Unzip and convert to geojson with bin/us-county-geojson.sh
  */
+ 
+var _ = require('highland'),
+    fs = require('fs'),
+    jsonStream = require('JSONStream'),
+    path = require('path'),
+    argv = require('optimist')
+      .demand('source')
+      .describe('source', 'source directory for year geojson files')
+      .argv;
+      
+var sourceDir = argv.source;
+
+// Read all files in the directory
+_(function(push, next){
+  fs.readdir(sourceDir, function(error, files){
+    if(error){
+      push(error);
+    } else {
+      for(var i = 0; i < files.length; i++){
+        push(null, files[i]);
+      }
+      push(null, _.nil);
+    }
+  });
+})
+
+// Only keep files that end on .geojson
+.filter(function(n){
+  return n.indexOf('.geojson') !== -1;
+})
+
+// Parse files with JSONStream
+.consume(function(error, filename, push, next){
+  if(filename === _.nil){
+    push(null, _.nil);
+    next();
+  } else {
+    console.log('processing %s', filename);
+    var year = filename.split('_')[2].split('.')[0];
+    fs.createReadStream(path.join(sourceDir, filename))
+      .pipe(jsonStream.parse(['features',true]))
+      .on('data', function(feature){
+        feature.properties.year = year;
+        push(null, feature);
+        //next();
+      })
+      .on('end', function(){
+        console.log('done parsing source file %s', filename);
+        next();
+      });
+  }
+})
+
+// Create place.jsons and associated .geojsons
+/*
+.consume(function(error, feature, push, next){
+  console.log(feature);
+  process.exit();
+})
+*/
+.pull(function(feature){
+  console.log(feature);
+  process.exit();
+});
+
+// Iterate over resulting place.jsons and .geojsons and send to OPD
