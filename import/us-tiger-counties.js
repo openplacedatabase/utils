0936d7ac807e5cbe3d70628795cef427aaf98cf4
Justin York <justin@genealogysystems.com>
2014-02-18 22:34:49 +0000

    Use async instead of highland

--- import/us-tiger-counties.js
+++ import/us-tiger-counties.js
@@ -5,7 +5,7 @@
  * 3. Unzip and convert to geojson with bin/us-county-geojson.sh
  */
  
-var _ = require('highland'),
+var async = require('async'),
     fs = require('fs'),
     jsonStream = require('JSONStream'),
     path = require('path'),
@@ -14,138 +14,132 @@ var _ = require('highland'),
       .describe('source', 'source directory for year geojson files')
       .demand('dest')
       .describe('dest', 'destination directory for saving temp OPD files')
+      .describe('fc', 'concurrency of saving features to disk')
+      .default('fc', 5)
+      .describe('ac', 'concurrency of saving features to OPD api')
+      .default('ac', 10)
       .argv;
-      
+
 var sourceDir = argv.source,
-    destDir = argv.dest;
-
-// Read all files in the directory
-_(function(push, next){
-  fs.readdir(sourceDir, function(error, files){
-    if(error){
-      push(error);
-    } else {
-      for(var i = 0; i < files.length; i++){
-        push(null, files[i]);
-      }
-      push(null, _.nil);
-    }
-  });
-})
-
-// Only keep files that end on .geojson
-.filter(function(filename){
-  return filename.indexOf('.geojson') !== -1;
-})
-
-// Extract features
-.flatMap(getFeatures)
-
-// Save features
-.consume(function(error, feature, push, next){
-  
-  // We're done saving all features
-  if(feature === _.nil){
-    push(null, _.nil);  
-  }
-  
-  // Save a feature
-  else {
-    
-    var filename = path.join(destDir, feature.properties.GISJOIN + '.json'),
-        year = feature.properties.year,
-        geometry = feature.geometry,
-        obj, geonum;
-        
-    fs.readFile(filename, function(error, data){
-      // File doesn't exist
-      if(error){
-        geonum = 1;
-        obj = {
-          names: {},
-          geojsons: {},
-          sources: ["Minnesota Population Center. National Historical Geographic Information System: Version 2.0. Minneapolis, MN: University of Minnesota 2011."]
-        };
-      } 
+    destDir = argv.dest,
+    fileConcurrency = argv.fc,
+    apiConcurrency = argv.ac;
       
-      // File already exists
-      else {
-        obj = JSON.parse(data);
-        geonum = Object.keys(obj.geojsons).length + 1;
-      }
-       
-      // Update the object
-      obj.names[year] = getName(feature);
-      obj.geojsons[year] = geonum;
-      
-      // Save the .json
-      saveObject(filename, obj, function(error){
-        if(error){
-          push(error);
-          next();
-        } else {
-          
-          // Save the .geojson
-          var geoFilename = path.join(destDir, feature.properties.GISJOIN + '.' + geonum + '.geojson');
-          saveObject(geoFilename, geometry, function(error){
-            if(error){
-              push(error);
-            }
-            next();
-          });
-        }
-      });
-    }); 
-  }
+async.auto({
 
-})
-
-// Iterate over resulting place.jsons and .geojsons and send to OPD
-.consume(function(error, x, push, next){
+  // Get a list of filenames that contain features
+  filenames: listFiles,
   
-  // We're done saving features so let's send all of
-  // the resulting OPD files to the server
-  if(x === _.nil){
-    console.log('TODO: send files to server');
-  } 
-  
-  // We shouldn't get here
-  else {
-    push(new Error('OPD -> server consume: we should not get a non nil object'));
-    next();
-  }
-})
+  // Extract and save features from the files
+  extract: ['filenames', extractFeatures]
 
-// Thunk
-.resume();
+}, function(error, results){
+  if(error){
+    console.error(error);
+  }
+  console.log('Finished');
+});    
 
 /**
- * Given a filename, extract and stream all features
+ * Get a list of .geojson files that we will
+ * extract GeoJSON features from
  */
-function getFeatures(filename){
+function listFiles(autoCallback, data){
+  fs.readdir(sourceDir, function(error, files){
+    if(error){
+      autoCallback(error);
+    } 
+    else {
+      var filtered = [];
+      for(var i = 0; i < files.length; i++){
+        if(files[i].indexOf('.geojson') !== -1) {
+          filtered.push(files[i]);
+        }
+      }
+      autoCallback(null, filtered);
+    }
+  });
+};
+
+/**
+ * Load a .geojson, extract the features, and save
+ * in an intermeddiate OPD format
+ */
+function extractFeatures(autoCallback, data){
+  async.eachSeries(data.filenames, function(filename, eachCallback){
   
-  var year = filename.split('_')[2].split('.')[0],
-      finished = false,
-      queue = [],
-      _push,
-      _next,      
-      featureStream = fs.createReadStream(path.join(sourceDir, filename))
-        .pipe(jsonStream.parse(['features',true]))
-        .on('data', function(feature){
-          featureStream.pause();
-          feature.properties.year = year;
-          _push(null, feature);
-          _next();
-        })
-        .on('end', function(){
-          _push(null, _.nil);
-          _next();
+    var year = filename.split('_')[2].split('.')[0];
+    var featureStream = fs.createReadStream(path.join(sourceDir, filename))
+          .pipe(jsonStream.parse(['features',true]));
+    var queue = async.queue(saveFeature, fileConcurrency);
+    
+    queue.drain = function(){
+      featureStream.resume();
+    };
+    
+    featureStream.on('data', function(feature){
+      featureStream.pause();
+      feature.properties.year = year;
+      queue.push(feature);
+    }).on('end', function(){
+      // We're done processing this file
+      eachCallback();
+    });
+  
+  }, function(){
+    // We're done processing all files
+    autoCallback();
+  });
+};
+
+/**
+ * Save a feature by creating/updating the place.json
+ * and the .geojson
+ */
+function saveFeature(feature, queueCallback){
+
+  var filename = path.join(destDir, feature.properties.GISJOIN + '.json'),
+      year = feature.properties.year,
+      geometry = feature.geometry,
+      obj, geonum;
+      
+  fs.readFile(filename, function(error, data){
+    // File doesn't exist
+    if(error){
+      geonum = 1;
+      obj = {
+        names: {},
+        geojsons: {},
+        sources: ["Minnesota Population Center. National Historical Geographic Information System: Version 2.0. Minneapolis, MN: University of Minnesota 2011."]
+      };
+    } 
+    
+    // File already exists
+    else {
+      obj = JSON.parse(data);
+      geonum = Object.keys(obj.geojsons).length + 1;
+    }
+     
+    // Update the object
+    obj.names[year] = getName(feature);
+    obj.geojsons[year] = geonum;
+    
+    // Save the .json
+    saveObject(filename, obj, function(error){
+      if(error){
+        console.error('Error saving %s', filename, error);
+      } else {
+        
+        // Save the .geojson
+        var geoFilename = path.join(destDir, feature.properties.GISJOIN + '.' + geonum + '.geojson');
+        saveObject(geoFilename, geometry, function(error){
+          if(error){
+            console.error('Error saving %s', geoFilename, error);
+          }
+          queueCallback();
         });
-  
-  return _(function(push, next){    
-    _push = push;
-    _next = next;
-    featureStream.resume();
+      }
+    });
   });
 };
 
