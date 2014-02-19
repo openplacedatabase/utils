a0715c746d7269ba8c5fa61260c026c62eb21969
Justin York <justin@genealogysystems.com>
2014-02-19 18:47:54 +0000

    Prepare place objects for saving via api

--- import/us-tiger-counties.js
+++ import/us-tiger-counties.js
@@ -7,8 +7,10 @@
  
 var async = require('async'),
     fs = require('fs'),
+    dir = require('node-dir'),
     jsonStream = require('JSONStream'),
     path = require('path'),
+    uuid = require('node-uuid'),
     argv = require('optimist')
       .demand('source')
       .describe('source', 'source directory for year geojson files')
@@ -31,7 +33,10 @@ async.auto({
   filenames: listFiles,
   
   // Extract and save features from the files
-  extract: ['filenames', extractFeatures]
+  extract: ['filenames', extractFeatures],
+  
+  // Save to OPD API
+  save: ['extract', savePlaces]
 
 }, function(error, results){
   if(error){
@@ -99,7 +104,7 @@ function extractFeatures(autoCallback, data){
 function saveFeature(feature, queueCallback){
 
   var filename = path.join(destDir, feature.properties.GISJOIN + '.json'),
-      year = feature.properties.year,
+      year = parseInt(feature.properties.year),
       geometry = feature.geometry,
       obj, geonum;
       
@@ -108,8 +113,8 @@ function saveFeature(feature, queueCallback){
     if(error){
       geonum = 1;
       obj = {
-        names: {},
-        geojsons: {},
+        names: [],
+        geojsons: [],
         sources: ["Minnesota Population Center. National Historical Geographic Information System: Version 2.0. Minneapolis, MN: University of Minnesota 2011."]
       };
     } 
@@ -121,8 +126,14 @@ function saveFeature(feature, queueCallback){
     }
      
     // Update the object
-    obj.names[year] = getName(feature);
-    obj.geojsons[year] = geonum;
+    obj.names.push({
+      year: year,
+      name: getName(feature)
+    });
+    obj.geojsons.push({
+      year: year,
+      id: geonum
+    });
     
     // Save the .json
     saveObject(filename, obj, function(error){
@@ -140,6 +151,78 @@ function saveFeature(feature, queueCallback){
         });
       }
     });
+  });
+};
+
+/**
+ * Send all resulting place and geojson files to the OPD API
+ */
+function savePlaces(autoCallback){
+  // Get a list of all .json files in the dest directory
+  dir.readFiles(destDir, { match: /\.json$/ }, function(error, content, filename, next){
+    
+    var place = JSON.parse(content);
+    
+    // Set the version
+    place.version = 1;
+    
+    // Generate a UUID
+    place.id = uuid.v4();
+    
+    //
+    // Calculate the name year spans
+    //
+    
+    // Sort the names by year
+    place.names.sort(function(a,b){
+      return a.year - b.year;
+    });
+    
+    // Process all but the last one
+    for(var i = 0; i < place.names.length - 1; i++){
+      var currentName = place.names[i],
+          nextName = place.names[i + 1];
+      currentName.from = currentName.year + '-01-01';
+      currentName.to = (nextName.year - 1) + '-12-31';
+      delete currentName.year;
+    }
+    
+    // Handle the last name; it's a special case
+    var lastName = place.names[place.names.length - 1];
+    lastName.from = lastName.year + '-01-01';
+    lastName.to = lastName.year + '-12-31';
+    delete lastName.year;
+    
+    //
+    // Calculate the geojson year spans
+    //
+    
+    // Sort the geojsons by year
+    place.geojsons.sort(function(a,b){
+      return a.year - b.year;
+    });
+    
+    // Process all but the last one
+    for(var i = 0; i < place.geojsons.length - 1; i++){
+      var currentGeo = place.geojsons[i],
+          nextGeo = place.geojsons[i + 1];
+      currentGeo.from = currentGeo.year + '-01-01';
+      currentGeo.to = (nextGeo.year - 1) + '-12-31';
+      delete currentGeo.year;
+    }
+    
+    // Handle the last geojson; it's a special case
+    var lastGeo = place.geojsons[place.geojsons.length - 1];
+    lastGeo.from = lastGeo.year + '-01-01';
+    lastGeo.to = lastGeo.year + '-12-31';
+    delete lastGeo.year;
+    
+    // Send the place to the server
+    
+    // Send the geojsons to the server
+    
+  }, function(error, files){
+    autoCallback(error);
   });
 };
 
