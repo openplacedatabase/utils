78856ae75fc4089b08250adcd1b7d743febb15ad
Justin York <justin@genealogysystems.com>
2014-02-20 23:58:01 +0000

    It works

---
+++ bin/nhgis-all-property-values.js
@@ -0,0 +1,72 @@
+/**
+ * Analyze an NHGIS .geojson feature collection for county
+ * borders of a given year. Collect and save all values
+ * of all properties.
+ */
+ 
+var _ = require('underscore')._,
+    high = require('highland'),
+    fs = require('fs'),
+    jsonStream = require('JSONStream'),
+    argv = require('optimist')
+      .demand('f')
+      .describe('f', 'source directory for year geojson files')
+      .argv;
+      
+var filename = argv.f,
+    properties = {};
+
+// Extract features
+getFeatures(filename).consume(function(error, feature, push, next){
+  
+  // We're done processing all features
+  if(feature === high.nil){
+    for(var p in properties){
+      properties[p] = _.uniq(properties[p]);
+    }
+    console.log(JSON.stringify(properties, null, 2));
+  }
+  
+  // Save properties
+  else {
+    for(var p in feature.properties){
+      if(_.isUndefined(properties[p])){
+        properties[p] = [];
+      }
+      properties[p].push(feature.properties[p]);
+    }
+    next();
+  }
+}).resume();
+
+/**
+ * Given a filename, extract and stream all features
+ */
+function getFeatures(filename){
+  
+  var finished = false,
+      queue = [],
+      _push = function(error, x){ queue.push(x); },
+      _next = function(){},      
+      featureStream = fs.createReadStream(filename)
+        .pipe(jsonStream.parse(['features',true]))
+        .on('data', function(feature){
+          featureStream.pause();
+          _push(null, feature);
+          _next();
+        })
+        .on('end', function(){
+          _push(null, high.nil);
+          _next();
+        });
+  
+  return high(function(push, next){    
+    _push = push;
+    _next = next;
+    for(var i = 0; i < queue.length; i++){
+      push(null, queue[i]);
+    }
+    queue = [];
+    featureStream.resume();
+  });
+};
---
+++ bin/nhgis-props-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Unzips a .zip download from NHGIS,
+# unzips all .zips inside, and runs
+# the ogr2ogr conversion on all resulting
+# .shp files to generate .geojson files
+
+# Make sure a valid directory was given as the first and second argument
+[[ -d $1 ]] || { 
+  echo "First argument must be a valid directory; '$1' given"; 
+  exit 1; 
+}
+[[ -d $2 ]] || { 
+  echo "Second argument must be a valid directory; '$2' given"; 
+  exit 1; 
+}
+
+# Unzip all resulting .zips if they haven't been already
+for inputfile in $(find $1 -type f -name "*.geojson"); do
+  echo "processing $inputfile"
+  outputfile=$(basename $inputfile)
+  node bin/nhgis-all-property-values.js -f $inputfile > $2/$outputfile
+done
